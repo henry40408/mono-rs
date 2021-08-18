9d545ae8c06669877ea9b5dd31b729911fba5257
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-08-18 23:49:33 +0800

    refactor: bring concurrency back

--- hcc/pushover/src/main.rs
+++ hcc/pushover/src/main.rs
@@ -24,6 +24,7 @@ use structopt::StructOpt;
 
 use hcc::CheckClient;
 use pushover::Notification;
+use std::sync::Arc;
 
 #[derive(Debug, StructOpt)]
 #[structopt(author, about)]
@@ -77,15 +78,26 @@ async fn check_domain_names(opts: &Opts, domain_names: &[&str]) -> anyhow::Resul
     let check_client = CheckClient::new();
     let results = check_client.check_certificates(domain_names)?;
 
-    for result in results {
-        let state_icon = result.state_icon(true);
-        let sentence = result.sentence();
-        let message = format!("{} {}", state_icon, sentence);
+    let mut futs = vec![];
 
-        let mut n = Notification::new(&opts.pushover_token, &opts.pushover_user, &message);
-        n.request.title = Some(format!("HTTP Certificate Check - {}", result.domain_name).into());
-        n.send().await?;
+    for result in results {
+        let r = Arc::new(result);
+        futs.push(async move {
+            let title = format!("HTTP Certificate Check - {}", r.domain_name);
+
+            let state_icon = r.state_icon(true);
+            let sentence = r.sentence();
+            let message = format!("{} {}", state_icon, sentence);
+
+            let mut n = Notification::new(&opts.pushover_token, &opts.pushover_user, &message);
+            n.request.title = Some(title.into());
+            n.send().await?;
+
+            Ok::<(), anyhow::Error>(())
+        });
     }
+
+    futures::future::try_join_all(futs).await?;
 
     Ok(())
 }
