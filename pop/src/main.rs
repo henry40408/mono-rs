699aacc4ac1e2c744d7b07bf4f2744e12fc82ae0
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-08-20 10:40:09 +0800

    refactor: replace actix-web with warp

--- pop/src/main.rs
+++ pop/src/main.rs
@@ -12,13 +12,18 @@
 
 //! Pop is proxy server to Pushover with attachment support
 
-use actix_web::{middleware, post, web, App, HttpRequest, HttpResponse, HttpServer};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::sync::Arc;
+
 use env_logger::Env;
 use log::warn;
-use pushover::{Attachment, Notification, Priority, Sound, HTML};
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
 use structopt::StructOpt;
+use warp::http::StatusCode;
+use warp::Filter;
+
+use pushover::{Attachment, Notification, Priority, Response, Sound, HTML};
 
 #[derive(StructOpt)]
 #[structopt(about, author)]
@@ -51,109 +56,118 @@ struct Message {
     image_url: Option<String>,
 }
 
-struct AppState {
-    authorization: Option<String>,
-    token: String,
-    user: String,
-}
-
 #[derive(Serialize)]
-struct ErrorMessage {
-    message: String,
+struct ErrorJson {
+    error: String,
 }
 
-fn get_authorization(req: &HttpRequest) -> Option<&str> {
-    req.headers().get("authorization")?.to_str().ok()
+enum AuthorizationState {
+    Public,
+    Accepted,
+    Rejected,
 }
 
-#[post("/1/messages")]
-async fn messages(
-    req: HttpRequest,
-    data: web::Data<AppState>,
-    message: web::Json<Message>,
-) -> HttpResponse {
-    if let Some(ref expected) = data.authorization {
-        if let Some(actual) = get_authorization(&req) {
-            if expected != actual {
-                // authorization and header are present but not equal
-                return HttpResponse::BadRequest().json(&ErrorMessage {
-                    message: "unauthorized".to_string(),
-                });
+fn check_authorization(expected: &Option<String>, actual: &Option<String>) -> AuthorizationState {
+    if let Some(e) = expected {
+        if let Some(a) = actual {
+            if a == e {
+                AuthorizationState::Accepted
             } else {
-                // authorization and header are present and equal
+                AuthorizationState::Rejected
             }
         } else {
-            // authorization is present but header is absent
-            return HttpResponse::BadRequest().json(&ErrorMessage {
-                message: "unauthorized".to_string(),
-            });
+            AuthorizationState::Rejected
         }
     } else {
-        // authorization is absent
-    }
-
-    let mut n = Notification::new(&data.token, &data.user, &message.message);
-
-    if let Some(ref d) = message.device {
-        n.request.device = Some(d.into());
-    }
-    if let Some(ref t) = message.title {
-        n.request.title = Some(t.into());
-    }
-    if let Some(ref h) = message.html {
-        n.request.html = Some(match HTML::from_str(h) {
-            Ok(h) => h,
-            Err(e) => return bad_request(e),
-        });
-    }
-
-    n.request.timestamp = message.timestamp;
-
-    if let Some(ref p) = message.priority {
-        n.request.priority = Some(match Priority::from_str(p) {
-            Ok(p) => p,
-            Err(e) => return bad_request(e),
-        });
-    }
-    if let Some(ref u) = message.url {
-        n.request.url = Some(u.into());
-        if let Some(ref t) = message.url_title {
-            n.request.url_title = Some(t.into());
-        }
-    }
-    if let Some(ref s) = message.sound {
-        n.request.sound = Some(match Sound::from_str(s) {
-            Ok(s) => s,
-            Err(e) => return bad_request(e),
-        });
-    }
-
-    let attachment;
-    if let Some(ref url) = message.image_url {
-        attachment = match Attachment::from_url(url).await {
-            Ok(a) => a,
-            Err(e) => return bad_request(e),
-        };
-        n.attach(&attachment);
-    }
-
-    let response = match n.send().await {
-        Ok(r) => r,
-        Err(e) => return bad_request(e),
-    };
-
-    if 1 == response.status {
-        HttpResponse::Ok().json(&response)
-    } else {
-        HttpResponse::BadRequest().json(&response)
+        AuthorizationState::Public
     }
 }
 
-fn bad_request<S: std::fmt::Debug>(e: S) -> HttpResponse {
-    HttpResponse::BadRequest().body(format!("{:?}", e))
+async fn send_notification(
+    opts: Arc<Opts>,
+    actual: Option<String>,
+    message: &Message,
+) -> Result<warp::reply::WithStatus<warp::reply::Json>, Infallible> {
+    match check_authorization(&opts.authorization, &actual) {
+        AuthorizationState::Rejected => Ok(warp::reply::with_status(
+            warp::reply::json(&ErrorJson {
+                error: "unauthorized".to_string(),
+            }),
+            StatusCode::UNAUTHORIZED,
+        )),
+        AuthorizationState::Public | AuthorizationState::Accepted => {
+            let n = Notification::new(&opts.token, &opts.user, &message.message);
+            match n.send().await {
+                Ok(r) => Ok(warp::reply::with_status(
+                    warp::reply::json(&r),
+                    StatusCode::OK,
+                )),
+                Err(e) => Ok(warp::reply::with_status(
+                    warp::reply::json(&ErrorJson {
+                        error: format!("{:?}", e),
+                    }),
+                    StatusCode::BAD_REQUEST,
+                )),
+            }
+        }
+    }
 }
 
-#[actix_web::main]
+fn with_opts(opts: Arc<Opts>) -> impl Filter<Extract = (Arc<Opts>,), Error = Infallible> + Clone {
+    warp::any().map(move || opts.clone())
+}
+
+fn one_messages_filter(
+    opts: Arc<Opts>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::reject::Rejection> + Clone {
+    warp::post()
+        .and(warp::path("1"))
+        .and(warp::path("messages"))
+        .and(with_opts(opts))
+        .and(warp::body::json())
+        .and(warp::header::optional::<String>("authorization"))
+        .and_then(
+            |opts: Arc<Opts>, message: Message, actual: Option<String>| async move {
+                send_notification(opts.clone(), actual, &message).await
+            },
+        )
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use mockito::mock;
+    use warp::http::Method;
+
+    use crate::one_messages_filter;
+
+    use super::Opts;
+
+    #[tokio::test]
+    async fn test_one_messages_filter() {
+        let _m = mock("POST", "/1/messages.json")
+            .with_status(200)
+            .with_body(r#"{"status":1,"request":"647d2300-702c-4b38-8b2f-d56326ae460b"}"#)
+            .create();
+        let opts = Arc::new(Opts {
+            token: "".to_string(),
+            user: "".to_string(),
+            authorization: None,
+            bind: "".to_string(),
+        });
+        let filter = one_messages_filter(opts);
+        let res = warp::test::request()
+            .method(Method::POST.as_str())
+            .path("/1/messages")
+            .body(r#"{"message":"test"}"#)
+            .reply(&filter)
+            .await;
+        assert_eq!(200, res.status());
+    }
+}
+
+#[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
@@ -162,21 +176,12 @@ async fn main() -> anyhow::Result<()> {
         warn!("no authorization set, server is vulnerable");
     }
 
-    let data = web::Data::new(AppState {
-        authorization: opts.authorization,
-        token: opts.token,
-        user: opts.user,
-    });
+    let opts = Arc::new(opts);
+    let filter = one_messages_filter(opts.clone());
+    let app = filter.with(warp::log("pop"));
 
-    HttpServer::new(move || {
-        App::new()
-            .app_data(data.clone())
-            .wrap(middleware::Logger::default())
-            .service(messages)
-    })
-    .bind(&opts.bind)?
-    .run()
-    .await?;
+    let bind: SocketAddr = opts.bind.parse()?;
+    warp::serve(app).bind(bind).await;
 
     Ok(())
 }
