f9d41afe0c20f58def521f799621777b7e3ed5d3
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-09-02 14:38:40 +0800

    refactor: exclude windows platform

--- wfs/src/main.rs
+++ wfs/src/main.rs
@@ -1,7 +1,21 @@
 use env_logger::Env;
+
+#[cfg(target_os = "windows")]
+use log::error;
+
+#[cfg(not(target_os = "windows"))]
 use log::info;
+
+#[cfg(not(target_os = "windows"))]
 use tokio::signal::unix::{signal, SignalKind};
 
+#[cfg(target_os = "windows")]
+fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    error!("windows is NOT supported");
+}
+
+#[cfg(not(target_os = "windows"))]
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
