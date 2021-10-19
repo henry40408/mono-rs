f75400691acf1068da497205c57d402f2f5c37e9
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-10-19 17:22:25 +0800

    feat: scrape

--- bk/src/main.rs
+++ bk/src/main.rs
@@ -1,3 +1,17 @@
+#![deny(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+
+//! Bookmark or bucket service
+
 fn main() {
     println!("Hello, world!");
 }
