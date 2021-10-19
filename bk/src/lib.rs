b4042b49bb8db40bc5e512fbb5748f3e13cb8804
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-10-19 17:31:15 +0800

    refactor: scrape URL and title

--- bk/src/lib.rs
+++ bk/src/lib.rs
@@ -14,15 +14,26 @@
 
 use headless_chrome::Browser;
 
+/// Scraped document
+#[derive(Debug)]
+pub struct Document<'a> {
+    url: &'a str,
+    title: String,
+    body: String,
+}
+
 /// Scrap HTML with URL
-pub fn scrape(url: &str) -> failure::Fallible<String> {
+pub fn scrape(url: &str) -> failure::Fallible<Document> {
     let browser = Browser::default()?;
     let tab = browser.wait_for_initial_tab()?;
     tab.navigate_to(url)?;
 
-    let body = tab.wait_for_element("html")?;
-    let s = body.get_inner_text()?;
-    Ok(s.trim().to_string())
+    let title_e = tab.wait_for_element("title")?;
+    let title = title_e.get_inner_text()?;
+
+    let body_e = tab.wait_for_element("html")?;
+    let body = body_e.get_inner_text()?;
+    Ok(Document { url, title, body })
 }
 
 #[cfg(test)]
@@ -31,8 +42,10 @@ mod test {
 
     #[test]
     fn test_scrape() -> failure::Fallible<()> {
-        let s = scrape("https://www.example.com")?;
-        assert!(s.contains("Example Domain"));
+        let doc = scrape("https://www.example.com")?;
+        assert_eq!("https://www.example.com", doc.url);
+        assert!(doc.title.contains("Example Domain"));
+        assert!(doc.body.contains("Example Domain"));
         Ok(())
     }
 }
