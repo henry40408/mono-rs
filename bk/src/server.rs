9415ca4ffd5def8fccbefdb59cd60ecb9fb0ebac
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-12-02 16:01:00 +0800

    refactor: bk integrates askama

--- bk/src/server.rs
+++ bk/src/server.rs
@@ -1,12 +1,21 @@
 use std::net::SocketAddr;
 use warp::Filter;
 
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html.j2")]
+struct IndexTemplate {}
+
 /// Start web server
 pub async fn start_server(bind: &str) -> anyhow::Result<()> {
     let bind: SocketAddr = bind.parse()?;
 
     let root = warp::path::end()
-        .map(|| format!("Ok"))
+        .map(|| {
+            let t = IndexTemplate {};
+            warp::reply::html(format!("{}", t.render().unwrap()))
+        })
         .with(warp::log("bk"));
 
     warp::serve(root).run(bind).await;
