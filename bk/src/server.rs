1f9dbbc958fd7cab53208930d3be5a1e998b6545
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-12-17 14:09:30 +0800

    refactor: extract handler and warp filter

--- bk/src/server.rs
+++ bk/src/server.rs
@@ -1,24 +1,27 @@
 use std::net::SocketAddr;
-use warp::Filter;
+use warp::{Filter, Rejection, Reply};
 
 use askama::Template;
+use bk::entities::Scrape;
 
 #[derive(Template)]
 #[template(path = "index.html.j2")]
 struct IndexTemplate {}
 
+fn index() -> impl Reply {
+    let t = IndexTemplate {};
+    warp::reply::html(format!("{}", t.render().unwrap()))
+}
+
+fn index_filter() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
+    warp::path::end().map(|| index())
+}
+
 /// Start web server
 pub async fn start_server(bind: &str) -> anyhow::Result<()> {
     let bind: SocketAddr = bind.parse()?;
-
-    let root = warp::path::end()
-        .map(|| {
-            let t = IndexTemplate {};
-            warp::reply::html(format!("{}", t.render().unwrap()))
-        })
-        .with(warp::log("bk"));
-
-    warp::serve(root).run(bind).await;
-
+    let root = index_filter();
+    let app = root.with(warp::log("bk"));
+    warp::serve(app).run(bind).await;
     Ok(())
 }
