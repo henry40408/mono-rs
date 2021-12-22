6aa3a6f2a420c7763804c2cc75df6deb2b2af95e
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-12-22 10:15:54 +0800

    refactor: get status code

--- bk/src/scraper.rs
+++ bk/src/scraper.rs
@@ -3,6 +3,7 @@ use failure::ResultExt;
 use headless_chrome::Browser;
 use reqwest::StatusCode;
 use scraper::{Html, Selector};
+use std::sync::{Arc, RwLock};
 
 /// Parameters for scrape
 #[derive(Debug)]
@@ -59,10 +60,21 @@ impl<'a> Scraper<'a> {
         let browser = Browser::default().compat()?;
         let tab = browser.wait_for_initial_tab().compat()?;
 
+        let status_code = Arc::new(RwLock::new(None));
+        let sc = status_code.clone();
+        let _result = tab.enable_response_handling(Box::new(move |params, _fetch_body| {
+            *sc.write().unwrap() = Some(params.response.status as i32);
+        }));
+
         tab.navigate_to(self.url).compat()?;
 
         // wait for initial rendering
         tab.wait_until_navigated().compat()?;
+
+        let http_status = match *status_code.read().unwrap() {
+            Some(c) => c,
+            None => bail!("failed to fetch status code"),
+        };
 
         let html_e = tab.wait_for_element("html").compat()?;
 
@@ -81,7 +93,13 @@ impl<'a> Scraper<'a> {
             .call_js_fn("function () { return document.title }", false)
             .compat()?;
         let title = match title_ro.value {
-            None => bail!("no title element found"),
+            None => {
+                if self.force {
+                    "".to_string()
+                } else {
+                    bail!("no title element found")
+                }
+            }
             Some(v) => serde_json::from_value::<String>(v)?,
         };
 
@@ -89,7 +107,7 @@ impl<'a> Scraper<'a> {
             params: self,
             title,
             html,
-            http_status: 0, // TODO get actual status code
+            http_status,
         }))
     }
 
@@ -121,7 +139,13 @@ impl<'a> Scraper<'a> {
             let selector = Selector::parse("title").unwrap();
 
             let title = match parsed.select(&selector).next() {
-                None => bail!("no title element found"),
+                None => {
+                    if self.force {
+                        "".to_string()
+                    } else {
+                        bail!("no title element found")
+                    }
+                }
                 Some(t) => t.text().collect::<Vec<_>>().join(""),
             };
             Ok(Scraped::Document(Document {
@@ -176,44 +200,69 @@ mod test {
     #[tokio::test]
     async fn test_scrape_with_headless_chromium() -> anyhow::Result<()> {
         let scraper = Scraper::from_url("https://www.example.com").with_headless(true);
-
         let scraped = scraper.scrape().await?;
         assert!(matches!(scraped, Scraped::Document(_)));
-
         if let Scraped::Document(doc) = scraped {
             assert_eq!("https://www.example.com", doc.params.url);
+            assert_eq!(200, doc.http_status);
             assert!(doc.title.contains("Example Domain"));
             assert!(doc.html.contains("Example Domain"));
         }
+        Ok(())
+    }
 
+    #[tokio::test]
+    async fn test_scrape_404_with_headless_chromium() -> anyhow::Result<()> {
+        let scraper = Scraper::from_url("https://httpbin.org/status/404")
+            .with_headless(true)
+            .with_force(true);
+        let scraped = scraper.scrape().await?;
+        assert!(matches!(scraped, Scraped::Document(_)));
+        if let Scraped::Document(doc) = scraped {
+            assert_eq!("https://httpbin.org/status/404", doc.params.url);
+            assert_eq!(404, doc.http_status);
+            assert_eq!("", doc.title);
+            assert_ne!("", doc.html);
+        }
         Ok(())
     }
 
     #[tokio::test]
     async fn test_scrape_wo_headless_chromium() -> anyhow::Result<()> {
         let scraper = Scraper::from_url("https://www.example.com");
-
         let scraped = scraper.scrape().await?;
         assert!(matches!(scraped, Scraped::Document(_)));
-
         if let Scraped::Document(doc) = scraped {
             assert_eq!("https://www.example.com", doc.params.url);
+            assert_eq!(200, doc.http_status);
             assert!(doc.title.contains("Example Domain"));
             assert!(doc.html.contains("Example Domain"));
         }
+        Ok(())
+    }
 
+    #[tokio::test]
+    async fn test_scrape_404_wo_headless_chromium() -> anyhow::Result<()> {
+        let scraper = Scraper::from_url("https://httpbin.org/status/404").with_force(true);
+        let scraped = scraper.scrape().await?;
+        assert!(matches!(scraped, Scraped::Document(_)));
+        if let Scraped::Document(doc) = scraped {
+            assert_eq!("https://httpbin.org/status/404", doc.params.url);
+            assert_eq!(404, doc.http_status);
+            assert!(doc.title.contains(""));
+            assert!(doc.html.contains(""));
+        }
         Ok(())
     }
 
     #[tokio::test]
     async fn test_scrape_image() -> anyhow::Result<()> {
         let scraper = Scraper::from_url("https://picsum.photos/1");
-
         let scraped = scraper.scrape().await?;
         assert!(matches!(scraped, Scraped::Blob(_)));
-
         if let Scraped::Blob(blob) = scraped {
             assert_eq!("https://picsum.photos/1", blob.params.url);
+            assert_eq!(200, blob.http_status);
             assert!(blob.content.len() > 0);
         }
 
