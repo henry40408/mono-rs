941fb0320f5450030bc01ac1f887acbbf040084f
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-11-26 01:31:24 +0800

    fix: cdu updates DNS records concurrently

--- cdu/src/lib.rs
+++ cdu/src/lib.rs
@@ -22,6 +22,7 @@ use anyhow::bail;
 use cloudflare::endpoints::dns::{DnsContent, DnsRecord};
 use cloudflare::endpoints::zone::Zone;
 use cloudflare::framework::response::ApiSuccess;
+use futures::stream::FuturesUnordered;
 use log::{debug, Level};
 use logging_timer::{finish, stimer};
 use moka::sync::Cache;
@@ -67,6 +68,46 @@ impl Display for Cached {
             Cached::IP(i) => write!(f, "{i}"),
         }
     }
+}
+
+async fn update_dns_record<'a, T>(
+    agent: Arc<Agent>,
+    token: T,
+    zone_id: T,
+    dns_record_id: T,
+    dns_record_name: T,
+    current_ip: Ipv4Addr,
+) -> anyhow::Result<()>
+where
+    T: Into<Cow<'a, str>>,
+{
+    let token = token.into();
+    let authorization = format!("bearer {token}");
+
+    let zone_id = zone_id.into();
+    let dns_record_name = dns_record_name.into();
+    let dns_record_id = dns_record_id.into();
+
+    let url = format!(
+        "{}/client/v4/zones/{zone_id}/dns_records/{dns_record_id}",
+        server_url()
+    );
+    let req = agent.put(&url).set("authorization", &authorization);
+    let tmr = stimer!(Level::Debug; "UPDATE_DNS_RECORD", "zone_id={zone_id},dns_record_id={dns_record_id}");
+    let res: ApiSuccess<DnsRecord> = req
+        .send_json(ureq::json!({
+            "type": "A",
+            "name":dns_record_name,
+            "content": current_ip,
+            "ttl": 1 // 1 for automatic
+        }))?
+        .into_json()?;
+    let content = match res.result.content {
+        DnsContent::A { content } => content.to_string(),
+        _ => "(not an A record)".into(),
+    };
+    finish!(tmr, "content={content}");
+    Ok(())
 }
 
 /// Cloudflare DNS Update
@@ -160,6 +201,8 @@ impl<'a> Cdu<'a> {
 
     /// Perform DNS record update on Cloudflare
     pub async fn run(&self) -> anyhow::Result<()> {
+        use futures::StreamExt as _;
+
         let tmr = stimer!(Level::Debug; "FETCH_IP_ADDRESS");
         let current_ip = public_ip::addr_v4().await.ok_or(NoIPV4)?;
         finish!(tmr, "current_ip={current_ip:?}");
@@ -180,61 +223,26 @@ impl<'a> Cdu<'a> {
             .get_record_identifiers(agent.clone(), &zone_id, &self.record_names)
             .await?;
 
-        let mut tasks = vec![];
+        let mut tasks = FuturesUnordered::new();
         for (id, name) in record_identifiers {
             let agent = agent.clone();
+            let token = self.token.to_string();
             let zone_id = zone_id.clone();
-            tasks.push(self.update_dns_record(agent, zone_id, id, name, current_ip));
+            tasks.push(tokio::spawn(async move {
+                update_dns_record(agent, token, zone_id, id, name, current_ip).await
+            }));
         }
 
         let len = tasks.len();
         let tmr = stimer!(Level::Debug; "UPDATE_DNS_RECORDS", "started={len}");
-        for task in futures::future::join_all(tasks).await {
-            task?;
+        while let Some(task) = tasks.next().await {
+            task??;
         }
         finish!(tmr, "finished={len}");
 
         // save current IP address when update succeeds
         self.cache.insert(CacheKey::LastIP, Cached::IP(current_ip));
 
-        Ok(())
-    }
-
-    async fn update_dns_record<T>(
-        &self,
-        agent: Arc<Agent>,
-        zone_id: T,
-        dns_record_id: T,
-        dns_record_name: T,
-        current_ip: Ipv4Addr,
-    ) -> anyhow::Result<()>
-    where
-        T: Into<Cow<'a, str>>,
-    {
-        let zone_id = zone_id.into();
-        let dns_record_name = dns_record_name.into();
-        let dns_record_id = dns_record_id.into();
-        let authorization = format!("bearer {}", &self.token);
-
-        let url = format!(
-            "{}/client/v4/zones/{zone_id}/dns_records/{dns_record_id}",
-            server_url()
-        );
-        let req = agent.put(&url).set("authorization", &authorization);
-        let tmr = stimer!(Level::Debug; "UPDATE_DNS_RECORD", "zone_id={zone_id},dns_record_id={dns_record_id}");
-        let res: ApiSuccess<DnsRecord> = req
-            .send_json(ureq::json!({
-                "type": "A",
-                "name":dns_record_name,
-                "content": current_ip,
-                "ttl": 1 // 1 for automatic
-            }))?
-            .into_json()?;
-        let content = match res.result.content {
-            DnsContent::A { content } => content.to_string(),
-            _ => "(not an A record)".into(),
-        };
-        finish!(tmr, "content={content}");
         Ok(())
     }
 }
@@ -287,8 +295,9 @@ mod tests {
             .create();
         let cdu = Cdu::new("token", "zone", &["record"]);
         let agent = Arc::new(cdu.build_agent());
-        cdu.update_dns_record(
+        update_dns_record(
             agent.clone(),
+            "token",
             "1",
             "2",
             "record",
